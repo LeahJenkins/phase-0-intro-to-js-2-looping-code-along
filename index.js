53b55b63c352fbcf43e729d3693fe97532dca6e5
Leah <leah.jenkins2019@gmail.com>
2023-01-11 11:47:09 -0500

    commit tree de11bafe94d479fe376837befc5ddbb1af4b26f1

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solutions in this file
+/* for ([initialization]; [condition]; [iteration]) {
+    [loop body]
+} */
+
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}*/
+/*const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    let i = 0
+    while (i < gifts.length){    
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        i++
+    }
+    return gifts
+
+}
+
+wrapGifts(gifts)*/
+/*const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        debugger
+    }
+    return gifts
+
+}
+
+wrapGifts(gifts)
+
+const cards = ["Charlie","Samip", "Ali", "birthday"]
+
+function writeCards(cards){
+
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`)
+        debugger
+    }
+    return cards
+}
+
+writeCards(cards)*/
+
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards){
+for (let i = 0; i < cards.length; i++){
+    console.log("Thank you, ${cards[i]} for the wonderful surprise gift!")
+}
+return cards
+}
+writeCards(cards)
